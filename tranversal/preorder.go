c2f9230cad85a1e3faaa4ab1725af65366bb6388
wuyan-dushang <1550693033@qq.com>
2024-07-02 22:20:06 +0800

    pre order

---
+++ tranversal/preorder.go
@@ -0,0 +1,28 @@
+package main
+
+import "container/list"
+
+func preorderTraversal(root *TreeNode) []int {
+	ans := []int{}
+
+	if root == nil {
+		return ans
+	}
+
+	st := list.New()
+	st.PushBack(root)
+
+	for st.Len() > 0 {
+		node := st.Remove(st.Back()).(*TreeNode)
+
+		ans = append(ans, node.Val)
+		if node.Right != nil {
+			st.PushBack(node.Right)
+		}
+		if node.Left != nil {
+			st.PushBack(node.Left)
+		}
+	}
+
+	return ans
+}
