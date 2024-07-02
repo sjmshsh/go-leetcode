7483d36a6ebf3ad8a5c979e8d8265a505ea0a3a9
wuyan-dushang <1550693033@qq.com>
2024-07-02 12:14:07 +0800

    shell_sort

--- sort/shell_sort.go
+++ sort/shell_sort.go
@@ -9,5 +9,14 @@ func main() {
 }
 
 func shellSort(s []int) []int {
-
+	// 不断缩小gap的值
+	for gap := len(s) / 2; gap > 0; gap = gap / 2 {
+		// 进行一个直接插入排序, 遍历每一个被gap分开的区间
+		for i := gap; i < len(s); i++ {
+			for j := i; j >= gap && s[j-gap] > s[j]; j = j - gap {
+				s[j], s[j-gap] = s[j-gap], s[j]
+			}
+		}
+	}
+	return s
 }
