55876316331fcf1cb756903fc73667dc4e012b2f
wuyan-dushang <1550693033@qq.com>
2024-07-05 20:49:20 +0800

    priority_queue

---
+++ 前k个高频元素/main.go
@@ -0,0 +1,55 @@
+package main
+
+import "container/heap"
+
+// 小根堆
+func topKFrequent(nums []int, k int) []int {
+	map_num := map[int]int{}
+	// 记录每个元素出现的次数
+	for _, item := range nums {
+		map_num[item]++
+	}
+	h := &IHeap{}
+	heap.Init(h)
+	// 将所有元素入堆，堆堆长度为k
+	for key, value := range map_num {
+		heap.Push(h, [2]int{key, value})
+		if h.Len() > k {
+			heap.Pop(h)
+		}
+	}
+	res := make([]int, k)
+	// 按顺序返回堆中的元素
+	for i := 0; i < k; i++ {
+		res[k-i-1] = heap.Pop(h).([2]int)[0]
+	}
+	return res
+}
+
+// 构建小根堆
+type IHeap [][2]int
+
+func (h IHeap) Len() int {
+	return len(h)
+}
+
+// 小根堆
+func (h IHeap) Less(i, j int) bool {
+	return h[i][1] < h[j][1]
+}
+
+func (h IHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *IHeap) Push(x interface{}) {
+	*h = append(*h, x.([2]int))
+}
+
+func (h *IHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
