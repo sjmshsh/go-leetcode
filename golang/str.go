a7c01b031bf9e5abb64c6d8f21f135b0c91227c5
wuyan-dushang <1550693033@qq.com>
2024-09-22 17:07:13 +0800

    add logic

---
+++ golang/str.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 原字符串
+	str := "hello"
+
+	// 将字符串转换为 rune 数组(适用于处理Unicode字符)
+	runeArray := []rune(str)
+
+	runeArray[1] = 'a'
+
+	modifiedStr := string(runeArray)
+
+	fmt.Println(modifiedStr)
+}
