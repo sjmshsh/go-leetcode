b2e26464f625ad7d452b0ac40239dd873aa36efe
wuyan-dushang <1550693033@qq.com>
2024-09-22 16:38:22 +0800

    add logic

--- golang/byte.go
+++ golang/byte.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
+	"fmt"
 )
 
 type MyType struct {
@@ -18,4 +19,18 @@ func MyTypeSliceToBytesSlice(data []MyType) ([]byte, error) {
 		}
 	}
 	return buf.Bytes(), nil
+}
+
+func main() {
+	// 零切片
+	zeroSlice := make([]int, 0)
+	fmt.Printf("zeroSlice: %v, len: %d, cap: %d, is nil: %t\n", zeroSlice, len(zeroSlice), cap(zeroSlice), zeroSlice == nil)
+
+	// 空切片
+	var emptySlice []int = []int{}
+	fmt.Printf("emptySlice: %v, len: %d, cap: %d, is nil: %t\n", emptySlice, len(emptySlice), cap(emptySlice), emptySlice == nil)
+
+	// nil 切片
+	var nilSlice []int
+	fmt.Printf("nilSlice: %v, len: %d, cap: %d, is nil: %t\n", nilSlice, len(nilSlice), cap(nilSlice), nilSlice == nil)
 }
